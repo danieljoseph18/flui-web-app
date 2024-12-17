af3d0d00d717ce2791836daabb536ebedb2b64d6
danieljoseph18 <danieljoseph1805@gmail.com>
2024-12-17 15:03:58 +0400

    Updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
+import path from "path";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config: any) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
